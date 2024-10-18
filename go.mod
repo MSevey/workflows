533adbb3ba15a877193f7271144195e823c64374
Matthew Sevey <15232757+MSevey@users.noreply.github.com>
2024-10-18 11:00:36 -0400

    add import

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MSevey/workflows
 
 go 1.23.1
+
+require github.com/MSevey/workflows-2 v0.0.0-20241018143835-b587d01c9d41 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"github.com/MSevey/workflows-2/hello"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("Hello, World!")
+	hello.Hello()
 }
